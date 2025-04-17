379af19ad1c395e9df51d791ceb07e5d7a869bd3
Felipe Batista <felipebatista47@hotmail.com>
2025-04-17 08:28:56 -0300

    note from api

---
+++ note.js
@@ -0,0 +1,18 @@
+
+{
+name: "Usuario Encarregado Teste",
+loginIdentifier: "encarregado@teste.com",
+password: "password123",
+role: "ENCARREGADO",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiRU5DQVJSRUdBRE8iLCJzdWIiOiIxIiwiaWF0IjoxNzQ0ODg3OTMyLCJleHAiOjE3NDU0OTI3MzJ9.s1uLi7urxMU35KVo3bCj8EfTtcZ7ABB0Gk2kmCtiqPE"
+},
+
+
+
+{
+  name: "Usuario Admin Teste",
+  loginIdentifier: "admin@teste.com",
+  password: "password123",
+  role: "ADMINISTRADOR",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQURNSU5JU1RSQURPUiIsInN1YiI6IjIiLCJpYXQiOjE3NDQ4ODgwMzMsImV4cCI6MTc0NTQ5MjgzM30.gGH8aCdxPzcW8H3O6BGonEsLztLNUaedGClY9Axu3-Y"
+}
